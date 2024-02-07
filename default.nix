e3e42ba1cae3fa4b8c83800af688ab44b78319d3
Sam <30577766+Samasaur1@users.noreply.github.com>
2024-02-06 19:53:37 -0800

    typo

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { neovimUtils, ... }:
 
 neovimUtils.buildNeovimPlugin {
-  pname = "rmeote-text-nvim";
+  pname = "remote-text-nvim";
   version = "0.1";
   src = ./.;
   meta.homepage = "https://github.com/Remote-Text/remote-text-neovim-client";
