92d5461b1c0df3519491e25da547acdfd6bd823b
Sam <30577766+Samasaur1@users.noreply.github.com>
2024-02-06 23:02:46 -0800

    add plenary dependency

--- default.nix
+++ default.nix
@@ -1,8 +1,9 @@
-{ vimUtils, ... }:
+{ vimUtils, vimPlugins, ... }:
 
 vimUtils.buildVimPlugin {
   pname = "remote-text-nvim";
   version = "0.1";
   src = ./.;
   meta.homepage = "https://github.com/Remote-Text/remote-text-neovim-client";
+  dependencies = with vimPlugins; [ plenary-nvim ];
 }
